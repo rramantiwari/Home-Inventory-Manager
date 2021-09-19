afad8b28b84a8c532b795f9daff799da8cf471e6
REVATIRAMAN DURGAPRASAD TIWARI <84346745+RRevatiaman@users.noreply.github.com>
2021-09-19 11:42:03 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,17 @@
+package homeInventory;
+
+public class InventoryItem {
+
+        public String description;
+        public String location;
+        public boolean marked;
+
+        public String serialNumber;
+        public String purchasePrice;
+        public String purchaseDate;
+        public String purchaseLocation;
+        public String note;
+        public String photoFile;
+
+
+}
